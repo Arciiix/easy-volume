c4771d7f90e9a8611956259618a0d6b9b4d92f46
Arciiix <artur.nowak100@gmail.com>
2023-07-16 20:01:07 +0200

    Write more tests

--- src/spec/volume.spec.ts
+++ src/spec/volume.spec.ts
@@ -1,5 +1,6 @@
 import { assert } from "console";
 import { getVolume, setVolume } from "..";
+import { SetVolumeError } from "../types";
 
 describe("volume", () => {
   let oldVolume = -1;
@@ -8,12 +9,18 @@ describe("volume", () => {
   it("gets volume", async () => {
     const volume = await getVolume();
     assert(volume !== -1);
+    assert(volume <= 100 && volume >= 0);
     assert(!isNaN(volume));
 
     console.log(`volume: ${volume}`);
     oldVolume = volume;
   });
-
+  it("handles invalid volume above", async () => {
+    expect(setVolume(110)).rejects.toThrow(SetVolumeError);
+  });
+  it("handles invalid volume below", async () => {
+    expect(setVolume(-1)).rejects.toThrow(SetVolumeError);
+  });
   it("sets volume without error", async () => {
     await setVolume(randomVolume);
   });
